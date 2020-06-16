5c3a9e0c802e8953b56f885371f444defac85df1
kawal18 <kanwalpreet1993@gmail.com>
2020-06-16 17:27:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<body>
-<h1> HEY WELCOME to my WEBSITE</h1>
-<div class="a1"> 
-<img src="https://kawal18899.s3.amazonaws.com/terrafrom_pic.jpg" height="300px" width="500px" >
-</div>
-
-</body>
-</html>
+Hi this is Kawal
